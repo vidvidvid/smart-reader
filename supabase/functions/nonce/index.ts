ff4cc8ca9f71155bf7da3f3fe5547a0857216fa9
Michael Young <michaelyoung.aero@gmail.com>
2023-08-02 12:05:07 -0400

    nonce function complete and tested

--- supabase/functions/nonce/index.ts
+++ supabase/functions/nonce/index.ts
@@ -5,7 +5,6 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 import { Database } from "../../lib/database.types.ts";
-// import { Database } from "lib/database.types";
 
 const SUPABASE_TABLE_USER = "users";
 serve(async (req) => {
@@ -14,22 +13,28 @@ serve(async (req) => {
     const { address } = await req.json();
     const nonce = Math.floor(Math.random() * 1000000);
     const supabase = createClient<Database>(
-        Deno.env.get(`SUPABASE_URL`) as string,
-        Deno.env.get(`SUPABASE_ANON_KEY`) as string
+        Deno.env.get("SUPABASE_URL") as string,
+        Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") as string // service role key required for row creation/editing
     );
-
-    await supabase
-        .from(SUPABASE_TABLE_USER)
-        .update({
-            auth: {
-                genNonce: nonce,
-                lastAuth: new Date().toISOString(),
-                lastAuthStatus: "pending",
+    // create or update with an nonce and lastAuthStatus = pending so that they can sign the message in the browser
+    const reponse = await supabase.from(SUPABASE_TABLE_USER).upsert(
+        [
+            {
+                address: address,
+                auth: {
+                    genNonce: nonce,
+                    lastAuth: new Date().toISOString(),
+                    lastAuthStatus: "pending",
+                },
             },
-        })
-        .eq("address", address);
-
-    return new Response(JSON.stringify(nonce), {
+        ],
+        {
+            onConflict: "address",
+        }
+    );
+    console.log(reponse);
+    const data = { nonce: nonce };
+    return new Response(JSON.stringify(data), {
         headers: { "Content-Type": "application/json" },
     });
 });
