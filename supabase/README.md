dcedf60d130b4f222c72fef9ffc0dfde1ec787f7
Michael Young <michaelyoung.aero@gmail.com>
2023-08-02 10:00:56 -0400

    nonce implemented

--- supabase/README.md
+++ supabase/README.md
@@ -1,5 +1,10 @@
 # Supabase backend
+rough readme for now
 
 ## setup
 
 install the supabase cli
+
+make sure you have docker running for local development
+
+
--- supabase/functions/nonce/index.ts
+++ supabase/functions/nonce/index.ts
@@ -3,14 +3,22 @@
 // This enables autocomplete, go to definition, etc.
 
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { Database } from "../../lib/database.types.ts";
+// import { Database } from "lib/database.types";
 
+const SUPABASE_TABLE_USER = "users";
 serve(async (req) => {
     // /api/nonce
 
     const { address } = await req.json();
     const nonce = Math.floor(Math.random() * 1000000);
+    const supabase = createClient<Database>(
+        Deno.env.get(`SUPABASE_URL`) as string,
+        Deno.env.get(`SUPABASE_ANON_KEY`) as string
+    );
 
-    await database
+    await supabase
         .from(SUPABASE_TABLE_USER)
         .update({
             auth: {
