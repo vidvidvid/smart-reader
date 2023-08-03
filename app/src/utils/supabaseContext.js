85a55803c2ab44af5c872be38714f48f77086dc7
Michael Young <michaelyoung.aero@gmail.com>
2023-08-03 12:05:21 -0400

    cleanup

--- app/src/utils/supabaseContext.js
+++ app/src/utils/supabaseContext.js
@@ -7,8 +7,6 @@ const SupabaseContext = createContext();
 const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
-// Create the client
-
 function SupabaseProvider({ children }) {
   const [token, setToken] = useState(null);
   const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
@@ -17,23 +15,12 @@ function SupabaseProvider({ children }) {
         Authorization: `Bearer ${token}`,
       },
     },
-    // This was in the documentation but doesn't work at all
-    // See: https://supabase.com/docs/guides/realtime/extensions/postgres-changes#custom-tokens
-    // realtime: {
-    //   headers: {
-    //     apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    //   },
-    //   params: {
-    //     apikey: accessToken,
-    //   },
-    // },
   });
 
   // Update headers whenever the token changes
   useEffect(() => {
     console.log('New token', token);
     if (token) {
-      // setCustomHeaders(token);
       supabase.realtime.accessToken = token;
     }
   }, [token]);
